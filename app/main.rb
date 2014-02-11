3f4a0f65d053992f890012712876e39276c6f3d6
Edward Harman <jaq+github@ethelred.org>
2014-02-11 16:15:24 -0500

    output tweaks

--- app/main.rb
+++ app/main.rb
@@ -37,8 +37,8 @@ shooters = [
 #        "Bikers, close, use bolters",
 #        Weapon.new(10, 4, 4, 5, TwinLinked.new)
 #    )      
-#     WeaponSet.new("Hive Guard old rules", Weapon.new(6, 4, 8, 4)),
-#     WeaponSet.new("Hive Guard new rules", Weapon.new(6, 3, 8, 4, IgnoreCover)),
+     WeaponSet.new("Hive Guard old rules", Weapon.new(6, 4, 8, 4)),
+     WeaponSet.new("Hive Guard new rules", Weapon.new(6, 3, 8, 4, IgnoreCover)),
 #     WeaponSet.new("Terminator squad (assault cannon)", Weapon.new(8, 4, 4, 5), Weapon.new(4, 4, 6, 4, Rending)),
      WeaponSet.new("Test lasgun", Weapon.new(1, 3, 3, 7)),
      WeaponSet.new("Test flamer", Weapon.new(1, 3, 4, 5, Template)),
@@ -46,11 +46,11 @@ shooters = [
      WeaponSet.new("Test lascannon", Weapon.new(1, 3, 9, 2)),
 ]
 targets = [
-#    Infantry.new("10 Space Marines", 10, 4, Save.new(3, 7, 4), 9),
-#    Infantry.new("10 Imperial Guard", 10, 3, Save.new(5, 7, 7), 8),
+    Infantry.new("10 Space Marines", 10, 4, Save.new(3, 7, 4), 9),
+    Infantry.new("10 Imperial Guard", 10, 3, Save.new(5, 7, 7), 8),
 #    Infantry.new("Test Target T3 no armour", 1, 3, Save.new(7, 7, 7), 7),
-#    Infantry.new("5 Terminators", 5, 4, Save.new(2, 5, 7), 9),
-#    Infantry.new("Daemon Prince", 4, 5, Save.new(3, 5, 4), 13),
+    Infantry.new("5 Terminators", 5, 4, Save.new(2, 5, 7), 9),
+    Infantry.new("Daemon Prince", 4, 5, Save.new(3, 5, 4), 13),
 #    Vehicle.new("Trukk Front", 1, 10),
     Vehicle.new("Rhino Front", 1, 11, 3),
     Vehicle.new("Sisters Rhino Front", 1, 11, 3, 1, Save.new(7, 6)),
@@ -68,7 +68,7 @@ targets.each do |target|
         end
         stats, events = target.result
         puts "#{weapons.name} vs. #{target.name}"
-        puts "Mean: #{stats.mean}, 80th percentile: #{stats.percentile(20)}"
+        printf "Mean: %.2f, 80th percentile: %.2f\n", stats.mean, stats.percentile(20)
         events.keys.sort.each do |evt|
             puts "#{evt}: #{100 * events[evt] / times}%"
         end
